ea2c9795df920cd07d1d54bf437f3a86fbad3b25
Ebru Engwall <ebru@skalelabs.com>
2021-05-05 09:20:53 -0700

    match game graph schema

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,20 +1,58 @@
-import { NewToken, UpdatedToken } from '../generated/MyToken/MyToken'
-import { MyToken } from '../generated/schema'
+import { TokenMinted } from '../generated/MemoryToken/MemoryToken'
+import { MyMemoryToken, Player, MyMemoryTokenMeta,Token } from '../generated/schema'
+import {  BigInt } from '@graphprotocol/graph-ts'
 
-export function handleNewToken(event: NewToken): void {
-  let myToken = new MyToken(event.params.id.toHex())
-  myToken.owner = event.params.owner
-  myToken.tokenValue = event.params.tokenValue
-  myToken.save()
-}
+let ONE = BigInt.fromI32(1)
 
-export function handleUpdatedToken(event: UpdatedToken): void {
-  let id = event.params.id.toHex()
-  let myToken = MyToken.load(id)
-  if (myToken == null) {
-    myToken = new MyToken(id)
+
+export function handleNewMemoryToken(event: TokenMinted): void {
+  let id = (event.params.from).toString().concat(event.params.tokenURI.toString())
+
+  let tokenMinted = new MyMemoryToken(id)
+  tokenMinted.from = event.params.from
+  tokenMinted.tokenURI = event.params.tokenURI
+
+  let player = Player.load(event.params.from.toString())
+  let token = Token.load(event.params.tokenURI.toString())
+
+  if (player != null) {
+    player.totalCountTokens = player.totalCountTokens.plus(ONE)
+  } else {
+    player = new Player(event.params.from.toString())
+    player.totalCountTokens = ONE
+    let tokenMeta = MyMemoryTokenMeta.load(ONE.toString())
+
+    if (tokenMeta == null) {
+      tokenMeta = new MyMemoryTokenMeta(ONE.toString())
+      tokenMeta.totalPlayerCount = ONE
+    } else {
+      tokenMeta.totalPlayerCount = tokenMeta.totalPlayerCount.plus(ONE)
+    }
+    tokenMeta.save()
+
+    if (token != null)
+      token.totalCountPlayers = token.totalCountPlayers.plus(ONE)
   }
-  myToken.owner = event.params.owner
-  myToken.tokenValue = event.params.tokenValue
-  myToken.save()
+
+  if(token == null)
+  {
+    token = new Token(tokenMinted.tokenURI.toString())
+    token.totalCountPlayers = ONE
+    token.howManyFound = ONE
+  }
+  else{
+    token.howManyFound = token.howManyFound.plus(ONE)
+  }
+
+  token.tokenURI = tokenMinted.tokenURI;
+  player.from = event.params.from
+  player.save()
+  token.save()
+  // token.save()
+
+  tokenMinted.token = token.id
+  tokenMinted.player = player.id
+  tokenMinted.save()
+
 }
+
